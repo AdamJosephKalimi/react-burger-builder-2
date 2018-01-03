c3ff43b581951da525a460bf4c788e63b209da4f
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-04 00:57:54 +0800

    Made the ingredients render dynamically within the burger component. The solution for mapping the ingredients object into an array that would allow the key value pairs to be accessible was very complex and needs to be reviewed.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,23 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  // This is a challenging concept
+  // Transforming an object of key: value pairs into
+  // an array of burger ingredients where the value of that object (in BurgerBuilder.js)
+  // is important to decide how many ingredients are needed
+  // and the Key is important for which type of ingredient
+  // is needed.
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+
+    });
   return (
       <div className={classes.Burger}>
           <BurgerIngredient type="bread-top" />
-          <BurgerIngredient type="cheese" />
-          <BurgerIngredient type="meat" />
+          {transformedIngredients}
           <BurgerIngredient type="bread-bottom" />
       </div>
     );
