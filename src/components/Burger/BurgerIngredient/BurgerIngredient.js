8febbd8c7c46468a637c7bccd1961a400b162e41
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-03 21:53:32 +0800

    propType validation works and the dev server runs

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './burgerIngredient.css';
+import PropTypes from 'prop-types';
 
-const burgerIngredient = (props) => {
-    let ingredient = null;
+class BurgerIngredient extends Component {
+    render () {
+      let ingredient = null;
 
-    switch (props.type) {
+    switch (this.props.type) {
       case ('bread-bottom'):
         ingredient = <div className={classes.BreadBottom}></div>;
         break;
@@ -33,6 +35,11 @@ const burgerIngredient = (props) => {
 
     return ingredient;
 
+    }
+}
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
 };
 
-export default burgerIngredient;
+export default BurgerIngredient;
