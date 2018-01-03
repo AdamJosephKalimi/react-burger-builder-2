caa4329136d8e7c87e5aeb72e0a9c534b6865fed
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-04 01:17:51 +0800

    added a message if there are no ingredients selected. Read .reduce and .map methods for js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -9,13 +9,21 @@ const burger = (props) => {
   // is important to decide how many ingredients are needed
   // and the Key is important for which type of ingredient
   // is needed.
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
       });
 
-    });
+    })
+    // Look up documentation on .map and .reduce and .concat
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+  if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+  console.log(transformedIngredients);
   return (
       <div className={classes.Burger}>
           <BurgerIngredient type="bread-top" />
