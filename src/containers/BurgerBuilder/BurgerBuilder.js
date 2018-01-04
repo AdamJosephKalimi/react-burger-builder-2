c84f7cb3fc0d2f7306180592052d6ddb9b73e833
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-04 17:34:24 +0800

    User can now successfully add ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,16 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+// Name constants you want to use as global constants
+// using ALLCAPS
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.7,
+    cheese: 0.4,
+    meat: 1.3
+};
+
 class BurgerBuilder extends Component {
 
 // The ingredients here is an object
@@ -14,13 +24,32 @@ class BurgerBuilder extends Component {
         bacon: 0,
         cheese: 0,
         meat: 0
-      }
+      },
+      totalPrice: 4
+    }
+
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+
+        // Try refactoring this on your own into
+        // less lines
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
     render () {
         return (
               <Aux>
                   <Burger ingredients={this.state.ingredients}/>
-                  <BuildControls />
+                  <BuildControls
+                      ingredientAdded={this.addIngredientHandler}/>
               </Aux>
         );
     }
