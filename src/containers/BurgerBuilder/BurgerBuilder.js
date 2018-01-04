a87ee0924b0e06852c3852689675692adfd343d2
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-04 18:03:17 +0800

    Most of the logic is in removeIngredientHandler and BuildControl / BuildControls around 'disabled'.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -44,12 +44,39 @@ class BurgerBuilder extends Component {
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+          return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+
+        // Try refactoring this on your own into
+        // less lines
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    };
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
               <Aux>
                   <Burger ingredients={this.state.ingredients}/>
                   <BuildControls
-                      ingredientAdded={this.addIngredientHandler}/>
+                      ingredientAdded={this.addIngredientHandler}
+                      ingredientRemoved={this.removeIngredientHandler}
+                      disabled={disabledInfo}/>
               </Aux>
         );
     }
