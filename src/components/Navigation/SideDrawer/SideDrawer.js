fbd658ca0742bd5e424595f6c4f44506ff768c3e
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-06 16:23:56 +0800

    The sideDrawer can now close by clicking on the backdrop. Users still cannot control how to open it though a button click yet.

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,40 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css'
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
 
+  // Remember, this is not where we are controlling how
+  // the sideDrawer gets opened, only how it closes.
+  // How it opens will be controlled though clicking
+  // on the Menu btn
+
+
+  // Here, if the props.open is true, render classes.Open, which
+  // will open the SideDrawer. Otherwise attachedClasses is false
+  // and the sideDrawer is showing with the backdrop behind it
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+  // We use join in {attachedClasses.join(' ')} bc you cannot
+  // pass an array of strings in there- only a string.
+  // So it's joined with a space.
   return (
-      <div className={classes.SideDrawer} style={{height: props.height}}>
-          <div className={classes.Logo}>
-              <Logo />
+      <Aux>
+          <Backdrop show={props.open} clicked={props.closed}/>
+
+          <div className={attachedClasses.join(' ')} style={{height: props.height}}>
+              <div className={classes.Logo}>
+                  <Logo />
+              </div>
+              <nav>
+                  <NavigationItems />
+              </nav>
           </div>
-          <nav>
-              <NavigationItems />
-          </nav>
-      </div>
+      </Aux>
     );
 
   };
