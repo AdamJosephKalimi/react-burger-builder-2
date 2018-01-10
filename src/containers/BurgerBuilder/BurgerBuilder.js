5575addddd41bc47d0944018008ba8b884a5e03d
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-10 20:03:29 +0800

    Now the ingredients are going to be rendered from the backend. We also put in contingency messages incase there are any errors loading the ingredients from the backend.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,16 +25,23 @@ class BurgerBuilder extends Component {
 // The ingredients here is an object
 // (Not an array and not a hash)
     state = {
-      ingredients: {
-        salad: 0,
-        bacon: 0,
-        cheese: 0,
-        meat: 0
-      },
+      ingredients: null,
       totalPrice: 4,
       purchasable: false,
       purchasing: false,
-      loading: false
+      loading: false,
+      error: false
+    }
+
+    // This is a good method for fetching data
+    componentDidMount () {
+        axios.get('https://burgerbuilder-bf99e.firebaseio.com/ingredients.json')
+            .then(response => {
+                this.setState({ingredients: response.data});
+            })
+            .catch(error => {
+                this.setState({error: true})
+            });
     }
 
     updatePurchaseState (ingredients) {
@@ -144,14 +151,35 @@ class BurgerBuilder extends Component {
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
-        let orderSummary = <OrderSummary
+
+        let orderSummary = null;
+        let burger = this.state.error ? <p>Ingredients can't be loaded</p> : <Spinner />;
+
+        if (this.state.ingredients) {
+            burger = (
+                <Aux>
+                    <Burger ingredients={this.state.ingredients}/>
+                    <BuildControls
+                        ingredientAdded={this.addIngredientHandler}
+                        ingredientRemoved={this.removeIngredientHandler}
+                        disabled={disabledInfo}
+                        purchasable={this.state.purchasable}
+                        ordered={this.purchaseHandler}
+                        price={this.state.totalPrice}/>
+                </Aux>
+            );
+            orderSummary = <OrderSummary
               ingredients={this.state.ingredients}
               price={this.state.totalPrice}
               purchaseCancelled={this.purchaseCancelHandler}
               purchaseContinued={this.purchaseContinueHandler}/>
+        }
         if (this.state.loading) {
             orderSummary = <Spinner />
         }
+
+
+
         return (
           // The OrderSummary should only rerender when the Modal is
           // showing. Not at any other time.
@@ -159,14 +187,7 @@ class BurgerBuilder extends Component {
                   <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {orderSummary}
                   </Modal>
-                  <Burger ingredients={this.state.ingredients}/>
-                  <BuildControls
-                      ingredientAdded={this.addIngredientHandler}
-                      ingredientRemoved={this.removeIngredientHandler}
-                      disabled={disabledInfo}
-                      purchasable={this.state.purchasable}
-                      ordered={this.purchaseHandler}
-                      price={this.state.totalPrice}/>
+                  {burger}
               </Aux>
         );
     }
