89df81673f4bfe432b783abdab97e4eb4a16ae5a
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-10 18:05:06 +0800

    added axios, connected to firebase, and implemented a spinner that displays while an order is being posted to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../..//components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 // Name constants you want to use as global constants
 // using ALLCAPS
@@ -29,7 +31,8 @@ class BurgerBuilder extends Component {
       },
       totalPrice: 4,
       purchasable: false,
-      purchasing: false
+      purchasing: false,
+      loading: false
     }
 
     updatePurchaseState (ingredients) {
@@ -101,7 +104,36 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-      alert('You are proceeding!');
+      this.setState( { loading: true } );
+      //alert('You are proceeding!');
+      const order = {
+          ingredients: this.state.ingredients,
+          // On production, calculate this on the server
+          // so users cannot manipulate it before it's
+          // sent to the server.
+          price: this.state.totalPrice,
+          customer: {
+              name: 'Adam Kalibaba',
+              address: {
+                street: '1213 Main Lane',
+                zipCode: '11213',
+                country: 'China'
+              },
+              email: 'Test@testease.com'
+          },
+          deliverMethod: 'fastest'
+      }
+
+      // orders will be the name of the node created
+      // in firebase
+      axios.post('/orders.json', order)
+        .then(response => {
+          this.setState({ loading: false, purchasing: false });
+        })
+        .catch(error => {
+          this.setState({ loading: false, purchasing: false });
+        });
+
     }
     render () {
         const disabledInfo = {
@@ -110,16 +142,20 @@ class BurgerBuilder extends Component {
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
+        let orderSummary = <OrderSummary
+              ingredients={this.state.ingredients}
+              price={this.state.totalPrice}
+              purchaseCancelled={this.purchaseCancelHandler}
+              purchaseContinued={this.purchaseContinueHandler}/>
+        if (this.state.loading) {
+            orderSummary = <Spinner />
+        }
         return (
           // The OrderSummary should only rerender when the Modal is
           // showing. Not at any other time.
               <Aux>
                   <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                      <OrderSummary
-                          ingredients={this.state.ingredients}
-                          price={this.state.totalPrice}
-                          purchaseCancelled={this.purchaseCancelHandler}
-                          purchaseContinued={this.purchaseContinueHandler}/>
+                    {orderSummary}
                   </Modal>
                   <Burger ingredients={this.state.ingredients}/>
                   <BuildControls
