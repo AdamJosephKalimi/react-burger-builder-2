c5997c5f068c4a3489435daf1b365279be32a630
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-13 15:53:57 +0800

    Routing has been added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
+import { Route, Switch } from 'react-router-dom';
+
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 
@@ -8,8 +10,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/" component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
