e21f3d6a26b336255ff505a024144896c54c274d
Adam Kalimi <adam.kalimi@yahoo.com>
2018-01-13 20:09:02 +0800

    Orders are now being fetched, but still need to be displayed

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        order: [],
+        loading: true
+
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+              console.log('res.data');
+              const fetchedOrders = [];
+              for (let key in res.data) {
+                  fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                  });
+              }
+              this.setState({loading: false, orders: fetchedOrders});
+            })
+            .catch(err => {
+                this.setState({loading: false});
+            });
+    }
+
     render () {
         return (
             <div>
@@ -12,4 +38,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
